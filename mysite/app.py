0247e763fc72e2149f09858f50adcea008c38380
Sarath Sajan <sarathsajan1998@gmail.com>
2022-01-23 01:31:01 +0530

    created basic routes and static files

--- mysite/app.py
+++ mysite/app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return("Forza Horizon share-codes")
+    return render_template('home.html')
 
-app.run(debug=True)
+@app.route("/explore")
+def submit():
+    return ("top 5 share-code of past 1 day, 1 week, 1 month for each category like routes, livery, tune etc")
+
+@app.route("/search")
+def submit():
+    return ("share-code search form")
+
+@app.route("/submit")
+def submit():
+    return ("share-code submission form")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
