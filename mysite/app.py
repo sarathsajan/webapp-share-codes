4c2c3c6d803997419caae9f8caad57234a217501
Sarath Sajan <sarathsajan1998@gmail.com>
2022-01-22 20:02:05 +0530

    created app.py

--- mysite/app.py
+++ mysite/app.py
@@ -1,1 +1,9 @@
-# project will be built with python and flask web framework
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return("Forza Horizon share-codes")
+
+app.run(debug=True)
